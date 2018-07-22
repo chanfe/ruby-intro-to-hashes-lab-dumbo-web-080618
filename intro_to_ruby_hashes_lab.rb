9f23e5398d48ae61652d3ad54b9a1b4fad8a58eb
Felix Chan <felixchan29@gmail.com>
2018-07-22 00:25:06 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -40,9 +40,9 @@ def monopoly_with_third_tier
 	    },
 	    :rent_in_dollars => {
 	      :one_piece_owned => 25,
-	      :two_piece_owned => 50,
-	      :three_piece_owned => 100,
-	      :four_piece_owned => 200
+	      :two_pieces_owned => 50,
+	      :three_pieces_owned => 100,
+	      :four_pieces_owned => 200
 	    }
 	  }
 	}
@@ -69,9 +69,9 @@ def monopoly_with_fourth_tier
 	    },
 	    :rent_in_dollars => {
 	      :one_piece_owned => 25,
-	      :two_piece_owned => 50,
-	      :three_piece_owned => 100,
-	      :four_piece_owned => 200
+	      :two_piece_owneds => 50,
+	      :three_piece_owneds => 100,
+	      :four_piece_owneds => 200
 	    }
 	  }
 	}
